5f371dd1a11655fc26287d7f38e637800554c79e
Nick <nickbadger1980@googlemail.com>
2019-12-27 15:39:49 +0000

    Added yq

--- .config/yadm/bootstrap.d/030_k8s_bins.sh
+++ .config/yadm/bootstrap.d/030_k8s_bins.sh
@@ -4,6 +4,7 @@ KUSTOMIZE_VERSION=3.2.1
 K3D_VERSION=1.3.4
 STERN_VERSION=1.11.0
 CONFTEST_VERSION=0.15.0
+YQ_VERSION=2.4.1
 
 mkdir -p ${HOME}/bin
 
@@ -12,6 +13,7 @@ curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffo
 curl -Lo k3d https://github.com/rancher/k3d/releases/download/v${K3D_VERSION}/k3d-linux-amd64 && chmod +x k3d && mv k3d $HOME/bin
 curl -Lo stern https://github.com/wercker/stern/releases/download/{$STERN_VERSION}/stern_linux_amd64 && chmod +x stern && mv stern $HOME/bin
 curl -Lo /tmp/conftest.tar.gz https://github.com/instrumenta/conftest/releases/download/v${CONFTEST_VERSION}/conftest_${CONFTEST_VERSION}_Linux_x86_64.tar.gz && tar zxvf /tmp/conftest.tar.gz && chmod +x conftest && mv conftest $HOME/bin
+curl -Lo yq https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 && chmod +x yq && mv yq $HOME/bin
 
 export GO111MODULE=on
 go get sigs.k8s.io/kustomize/kustomize/v3@v${KUSTOMIZE_VERSION}
