2f8393cb34ac1b4fb381c91dd6da3ad6762e28e8
Nick <nickbadger1980@googlemail.com>
2019-12-27 15:50:44 +0000

    Install kustomize via release (not git-build)

--- .config/yadm/bootstrap.d/030_k8s_bins.sh
+++ .config/yadm/bootstrap.d/030_k8s_bins.sh
@@ -14,9 +14,8 @@ curl -Lo k3d https://github.com/rancher/k3d/releases/download/v${K3D_VERSION}/k3
 curl -Lo stern https://github.com/wercker/stern/releases/download/{$STERN_VERSION}/stern_linux_amd64 && chmod +x stern && mv stern $HOME/bin
 curl -Lo /tmp/conftest.tar.gz https://github.com/instrumenta/conftest/releases/download/v${CONFTEST_VERSION}/conftest_${CONFTEST_VERSION}_Linux_x86_64.tar.gz && tar zxvf /tmp/conftest.tar.gz && chmod +x conftest && mv conftest $HOME/bin
 curl -Lo yq https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 && chmod +x yq && mv yq $HOME/bin
-
+curl -Lo kustomize https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_kustomize.v${KUSTOMIZE_VERSION}_linux_amd64 && chmod +x kustomize && mv kustomize $HOME/bin
 export GO111MODULE=on
-go get sigs.k8s.io/kustomize/kustomize/v3@v${KUSTOMIZE_VERSION}
 go get github.com/mintel/k8s-yaml-splitter
 go get github.com/warrensbox/tgswitch
 go get github.com/warrensbox/terraform-switcher
