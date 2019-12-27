68c9436dd2fd55014a1bc569f645675b1f558b52
Nick <nickbadger1980@googlemail.com>
2019-12-27 13:04:17 +0000

    Added conf test

--- .config/yadm/bootstrap.d/030_k8s_bins.sh
+++ .config/yadm/bootstrap.d/030_k8s_bins.sh
@@ -3,6 +3,7 @@ KUBECTL_VERSION=1.17.0
 KUSTOMIZE_VERSION=3.2.1
 K3D_VERSION=1.3.4
 STERN_VERSION=1.11.0
+CONFTEST_VERSION=0.15.0
 
 mkdir -p ${HOME}/bin
 
@@ -10,6 +11,7 @@ curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v${KU
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 &&  chmod +x skaffold && mv skaffold $HOME/bin
 curl -Lo k3d https://github.com/rancher/k3d/releases/download/v${K3D_VERSION}/k3d-linux-amd64 && chmod +x k3d && mv k3d $HOME/bin
 curl -Lo stern https://github.com/wercker/stern/releases/download/{$STERN_VERSION}/stern_linux_amd64 && chmod +x stern && mv stern $HOME/bin
+curl -Lo /tmp/conftest.tar.gz https://github.com/instrumenta/conftest/releases/download/v${CONFTEST_VERSION}/conftest_${CONFTEST_VERSION}_Linux_x86_64.tar.gz && tar zxvf /tmp/conftest.tar.gz && chmod +x conftest && mv conftest $HOME/bin
 
 export GO111MODULE=on
 go get sigs.k8s.io/kustomize/kustomize/v3@v${KUSTOMIZE_VERSION}
@@ -19,4 +21,3 @@ go get github.com/warrensbox/terraform-switcher
 go get github.com/prometheus/prometheus/cmd/promtool
 go get github.com/prometheus/alertmanager/cmd/amtool
 go get github.com/brancz/gojsontoyaml
-go get github.com/instrumenta/conftest
