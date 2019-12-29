39ebd6d3110ff6952a70005825881ddb36197a49
Nick <nickbadger1980@googlemail.com>
2019-12-29 17:03:25 +0000

    Updating screenlayouts.

--- .screenlayout/home-dell-dual.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --primary --mode 3840x2160 --pos 0x1440 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --mode 2560x1440 --pos 0x0 --rotate normal --output VIRTUAL1 --off
